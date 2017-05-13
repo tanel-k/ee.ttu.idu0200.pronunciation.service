956cd27a816a24440587ccf61ccff3afe20f9f68
Tanel Prikk <tanel.prikk@outlook.com>
2017-05-13 13:01:22 +0300

    fixed; ready

--- src/main/java/ee/ttu/idu0200/pronunciation/service/Application.java
+++ src/main/java/ee/ttu/idu0200/pronunciation/service/Application.java
@@ -1,8 +1,5 @@
 package ee.ttu.idu0200.pronunciation.service;
 
-import java.util.Date;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -11,13 +8,12 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
 
-import ee.ttu.idu0200.pronunciation.service.model.WordContainer;
-import ee.ttu.idu0200.pronunciation.service.repository.WordContainerRepository;
-
 @SpringBootApplication
 public class Application implements CommandLineRunner {
+	/*
 	@Autowired
 	private WordContainerRepository wordRepository;
+	*/
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
@@ -25,7 +21,9 @@ public class Application implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
+		/*
 		wordRepository.deleteAll();
+		
 		WordContainer newContainer = new WordContainer();
 		newContainer.setWord("aabits");
 		newContainer.setLastModified(new Date());
@@ -35,18 +33,8 @@ public class Application implements CommandLineRunner {
 		newContainer.setWord("ahven");
 		newContainer.setLastModified(new Date());
 		wordRepository.save(newContainer);
+		*/
 	}
-
-	/*
-	 * implements CommandLineRunner:
-	 * 
-	 * @Autowired
-	 * private WordContainerRepository wordRepository;
-	 * @Override
-	 * public void run(String... args) throws Exception {
-	 *  wordRepository.deleteAll();
-	 * }
-	 */
 
 	@Bean
 	public CorsFilter corsFilter() {
--- src/main/java/ee/ttu/idu0200/pronunciation/service/controller/WordContainerController.java
+++ src/main/java/ee/ttu/idu0200/pronunciation/service/controller/WordContainerController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
@@ -41,7 +42,7 @@ public class WordContainerController {
 	}
 
 	@RequestMapping(value = "/words", method = RequestMethod.PUT)
-	public ResponseEntity<String> putWord(@RequestParam(name = "word") String word, @RequestParam(name = "pronunciation") MultipartFile pronunciation) {
+	public ResponseEntity<String> putWord(@RequestPart(name = "word") String word, @RequestPart(name = "pronunciation") MultipartFile pronunciation) {
 		if (pronunciation.isEmpty() || !AudioFileUtils.isMp3(pronunciation)) {
 			return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
 		}
